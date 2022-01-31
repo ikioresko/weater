eb82f81dc653c701502635e38dfbd0551d0c33e2
Igor Kioresko <ikioresko@gmail.com>
2022-01-31 11:35:31 +0500

    add admin role and panel, authority

--- src/main/java/com/weater/model/Role.java
+++ src/main/java/com/weater/model/Role.java
@@ -3,7 +3,7 @@ package com.weater.model;
 import org.springframework.security.core.GrantedAuthority;
 
 public enum Role implements GrantedAuthority {
-    USER;
+    USER, ADMIN;
 
     @Override
     public String getAuthority() {
