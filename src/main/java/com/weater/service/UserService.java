5ec374edc03a16a882b5e1481a278363c0bff97b
Igor Kioresko <ikioresko@gmail.com>
2022-02-02 15:52:57 +0500

    add email activation code service

--- src/main/java/com/weater/service/UserService.java
+++ src/main/java/com/weater/service/UserService.java
@@ -1,21 +1,57 @@
 package com.weater.service;
 
+import com.weater.model.Role;
+import com.weater.model.User;
 import com.weater.repos.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.UUID;
+
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepo userRepo;
+    private final MailSender mailSender;
 
-    public UserService(UserRepo userRepo) {
+    public UserService(UserRepo userRepo, MailSender mailSender) {
         this.userRepo = userRepo;
+        this.mailSender = mailSender;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if (userFromDb != null) {
+            return false;
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        user.setActivationCode(UUID.randomUUID().toString());
+        userRepo.save(user);
+        if (!user.getEmail().isEmpty()) {
+            String message = String.format("Hello, %s! \n" + "Welcome to Weater" +
+                            ". Please visit next link: http://localhost:8080/activate/%s",
+                    user.getUsername(),
+                    user.getActivationCode());
+            mailSender.send(user.getEmail(), "Activation code", message);
+        }
+        return true;
+    }
+
+    public boolean activateUser(String code) {
+        User user = userRepo.findByActivationCode(code);
+        if (user == null) {
+            return false;
+        }
+        user.setActivationCode(null);
+        userRepo.save(user);
+        return false;
     }
 }
