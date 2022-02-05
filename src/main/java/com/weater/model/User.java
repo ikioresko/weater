83fc2d57d3de4234eb94eceae8edf7792cead465
Igor Kioresko <ikioresko@gmail.com>
2022-02-05 20:13:06 +0500

    add subscribe and subscribers

--- src/main/java/com/weater/model/User.java
+++ src/main/java/com/weater/model/User.java
@@ -7,6 +7,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -35,6 +36,16 @@ public class User implements UserDetails {
     private Set<Role> roles;
     @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Message> messages;
+
+    @ManyToMany
+    @JoinTable(name = "user_subscriptions", joinColumns = {@JoinColumn(name = "chanel_id")},
+            inverseJoinColumns = {@JoinColumn(name = "subscriber_id")})
+    private Set<User> subscribers = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(name = "user_subscriptions", joinColumns = {@JoinColumn(name = "subscriber_id")},
+            inverseJoinColumns = {@JoinColumn(name = "chanel_id")})
+    private Set<User> subscriptions = new HashSet<>();
 
     public boolean isAdmin() {
         return roles.contains(Role.ADMIN);
@@ -122,6 +133,22 @@ public class User implements UserDetails {
     @Override
     public boolean isEnabled() {
         return isActive();
+    }
+
+    public Set<User> getSubscribers() {
+        return subscribers;
+    }
+
+    public void setSubscribers(Set<User> subscribers) {
+        this.subscribers = subscribers;
+    }
+
+    public Set<User> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public void setSubscriptions(Set<User> subscriptions) {
+        this.subscriptions = subscriptions;
     }
 
     @Override
---
+++ src/main/resources/db/migration/V5__Add_user.sql
@@ -0,0 +1,5 @@
+insert into usr (id, username, password, active)
+    values(2, 'user', '1', true);
+
+insert into user_role (user_id, roles)
+    values(2, 'USER');
